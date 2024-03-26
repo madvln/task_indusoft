1e8b6abf4e9b6d9de2c0f5e23cd38ddc9a3a59b0
Егор Тараканчиков <egor200050@gmail.com>
2024-03-26 21:51:45 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿using System.Windows.Forms;
+
+namespace task_indusoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.min_boundary = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.max_boundary = new System.Windows.Forms.TextBox();
+            this.check_tracing_boundaries = new System.Windows.Forms.CheckBox();
+            this.List_of_events = new System.Windows.Forms.ListBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox_time_from = new System.Windows.Forms.CheckBox();
+            this.checkBox_time_to = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sig1",
+            "Sig2",
+            "Sig3",
+            "Sig4"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Sig1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "List of signals";
+            // 
+            // min_boundary
+            // 
+            this.min_boundary.Location = new System.Drawing.Point(23, 139);
+            this.min_boundary.Name = "min_boundary";
+            this.min_boundary.Size = new System.Drawing.Size(102, 20);
+            this.min_boundary.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(19, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Signal boundaries";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Max";
+            // 
+            // max_boundary
+            // 
+            this.max_boundary.Location = new System.Drawing.Point(131, 139);
+            this.max_boundary.Name = "max_boundary";
+            this.max_boundary.Size = new System.Drawing.Size(102, 20);
+            this.max_boundary.TabIndex = 2;
+            // 
+            // check_tracing_boundaries
+            // 
+            this.check_tracing_boundaries.AutoSize = true;
+            this.check_tracing_boundaries.Location = new System.Drawing.Point(23, 165);
+            this.check_tracing_boundaries.Name = "check_tracing_boundaries";
+            this.check_tracing_boundaries.Size = new System.Drawing.Size(117, 17);
+            this.check_tracing_boundaries.TabIndex = 3;
+            this.check_tracing_boundaries.Text = "Tracing boundaries";
+            this.check_tracing_boundaries.UseVisualStyleBackColor = true;
+            // 
+            // List_of_events
+            // 
+            this.List_of_events.FormattingEnabled = true;
+            this.List_of_events.Location = new System.Drawing.Point(370, 64);
+            this.List_of_events.Name = "List_of_events";
+            this.List_of_events.Size = new System.Drawing.Size(310, 303);
+            this.List_of_events.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(370, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2024, 3, 26, 11, 52, 0, 0);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label5.Location = new System.Drawing.Point(334, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "From";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Location = new System.Drawing.Point(537, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "To";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "dd.MM.yyyy HH:mm:ss";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(573, 38);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(141, 20);
+            this.dateTimePicker2.TabIndex = 6;
+            this.dateTimePicker2.Value = new System.DateTime(2024, 3, 26, 21, 45, 0, 0);
+            // 
+            // checkBox_time_from
+            // 
+            this.checkBox_time_from.AutoSize = true;
+            this.checkBox_time_from.Location = new System.Drawing.Point(313, 38);
+            this.checkBox_time_from.Name = "checkBox_time_from";
+            this.checkBox_time_from.Size = new System.Drawing.Size(15, 14);
+            this.checkBox_time_from.TabIndex = 3;
+            this.checkBox_time_from.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_time_to
+            // 
+            this.checkBox_time_to.AutoSize = true;
+            this.checkBox_time_to.Location = new System.Drawing.Point(517, 38);
+            this.checkBox_time_to.Name = "checkBox_time_to";
+            this.checkBox_time_to.Size = new System.Drawing.Size(15, 14);
+            this.checkBox_time_to.TabIndex = 3;
+            this.checkBox_time_to.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.List_of_events);
+            this.Controls.Add(this.checkBox_time_to);
+            this.Controls.Add(this.checkBox_time_from);
+            this.Controls.Add(this.check_tracing_boundaries);
+            this.Controls.Add(this.max_boundary);
+            this.Controls.Add(this.min_boundary);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox min_boundary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox max_boundary;
+        private System.Windows.Forms.CheckBox check_tracing_boundaries;
+        private System.Windows.Forms.ListBox List_of_events;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private CheckBox checkBox_time_from;
+        private CheckBox checkBox_time_to;
+        private Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_indusoft
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadBoundariesFromDatabase();
+            LoadEventsFromDatabase();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 10000; // Установка интервала таймера в миллисекундах (1 секунда)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (check_tracing_boundaries.Checked)
+            {
+                // Генерация данных и запись в базу данных
+                GenerateAndSaveData();
+            }
+        }
+
+        private void LoadBoundariesFromDatabase()
+        {
+            string connectionString = @"Server=MSI-Y9\SQLEXPRESS;Database=base_test;Integrated Security=True;";
+            string query = "SELECT lowBorder, highBorder FROM Borders WHERE nameSignal = @nameSignal";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@nameSignal", comboBox1.SelectedItem?.ToString());
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        double lowBorder = reader.GetDouble(reader.GetOrdinal("lowBorder"));
+                        double highBorder = reader.GetDouble(reader.GetOrdinal("highBorder"));
+
+                        min_boundary.Text = lowBorder.ToString();
+                        max_boundary.Text = highBorder.ToString();
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading boundaries from the database: " + ex.Message);
+                }
+            }
+        }
+
+        private float GenerateRandomValue()
+        {
+            Random random = new Random();
+            return (float)random.NextDouble() * 200; // Пример случайного значения
+        }
+
+        private void GenerateAndSaveData()
+        {
+            float value = GenerateRandomValue();
+
+            // Получение имени сигнала из выпадающего списка
+            string selectedSignal = comboBox1.SelectedItem?.ToString();
+
+            if (selectedSignal != null)
+            {
+                bool isTracingBoundariesEnabled = check_tracing_boundaries.Checked;
+                if (isTracingBoundariesEnabled)
+                {
+                    float minBoundary = float.Parse(min_boundary.Text);
+                    float maxBoundary = float.Parse(max_boundary.Text);
+                    if (value < minBoundary || value > maxBoundary)
+                    {
+                        // Запись данных в базу данных
+                        string connectionString = @"Server=MSI-Y9\SQLEXPRESS;Database=base_test;Integrated Security=True;";
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            string query = "INSERT INTO TimeSeries (nameSignal, value, timeStamp) VALUES (@nameSignal, @value, @timeStamp)";
+                            SqlCommand command = new SqlCommand(query, connection);
+                            command.Parameters.AddWithValue("@nameSignal", selectedSignal);
+                            command.Parameters.AddWithValue("@value", value);
+                            command.Parameters.AddWithValue("@timeStamp", DateTime.Now);
+                            command.ExecuteNonQuery();
+
+                            LoadEventsFromDatabase();
+                        }
+                    } 
+                }
+            }
+        }
+
+        // Метод для загрузки событий из базы данных
+        private void LoadEventsFromDatabase()
+        {
+            // Получение даты и времени из dateTimePicker1
+            DateTime fromDate = dateTimePicker1.Value;
+
+            // Получение даты и времени из dateTimePicker2
+            DateTime toDate = dateTimePicker2.Value;
+
+            // Проверка состояния флагов checkBox_time_from и checkBox_time_to
+            if (!checkBox_time_from.Checked)
+            {
+                // Если флаг не установлен, используем минимальную дату
+                fromDate = new DateTime(1753, 1, 1);
+            }
+
+            if (!checkBox_time_to.Checked)
+            {
+                // Если флаг не установлен, используем максимальную дату
+                toDate = new DateTime(9999, 12, 31, 23, 59, 59);
+            }
+
+            // Очищаем ListBox перед загрузкой новых данных
+            List_of_events.Items.Clear();
+            // Подключение к базе данных
+            string connectionString = @"Server=MSI-Y9\SQLEXPRESS;Database=base_test;Integrated Security=True;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Создание команды для выполнения SQL-запроса
+                string query = "SELECT * FROM TimeSeries WHERE timeStamp >= @fromDate AND timeStamp <= @toDate";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@fromDate", fromDate);
+                command.Parameters.AddWithValue("@toDate", toDate);
+
+                // Выполнение команды и получение данных
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    // Получение значений из результата запроса
+                    string nameSignal = reader["nameSignal"].ToString();
+                    DateTime time = (DateTime)reader["timeStamp"];
+                    double generatedValue = (double)reader["value"];
+
+                    // Отображение данных в списке событий
+                    List_of_events.Items.Add($"{time}: Signal '{nameSignal}' generated value {generatedValue}");
+                }
+                reader.Close();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            LoadEventsFromDatabase();
+        }
+    }
+}
